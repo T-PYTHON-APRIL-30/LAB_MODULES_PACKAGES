6011b2b5c4b1fd1dd0fd9d80e3a5f39b0e507207
SulimanBinM <sbinmohammed_1@icloud.com>
2023-05-09 17:16:09 +0300

    Start the bonus.

---
+++ Library/librarian.py
@@ -0,0 +1,20 @@
+
+library = {"library:":"King Fahad library","Title":"Think python"}
+
+
+
+
+def add_book(libra:str, title:str, author:str, isbn:str):
+    pass
+
+def remove_book(library, isbn):
+    pass
+
+def check_out_book(library, isbn):
+    pass
+
+def return_book(library, isbn):
+    pass
+
+def display_books(library):
+    pass
