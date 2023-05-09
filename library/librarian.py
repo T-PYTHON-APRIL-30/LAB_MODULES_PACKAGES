377c677e2fcebf8aa734ca201c16905f3eacb927
amani <amanii1994@outlook.com>
2023-05-09 23:49:28 +0300

    Modules LAB + Bonus LAB

--- library/librarian.py
+++ library/librarian.py
@@ -1,51 +1,53 @@
-
+counter = 1
 
 def add_book(library:dict, title:str, author:str, isbn:str):
     '''takes a dictionary (library), a title (string), an author (string), and an ISBN (string) as input,
     and adds a new book to the library as a dictionary with the keys 'title', 'author', 'isbn', and 'available'.
     The 'available' key should have a boolean value, initially set to True. If the ISBN already exists in the library,
       print an appropriate message.'''
-    counter = 1
+    global counter
     library[counter] = {'Title':f'{title}','Author':f'{author}','ISBN':f'{isbn}','Available':True}
     counter +=1
     print('The book added successfully..')
 
+    
+
 def remove_book(library, isbn):
     '''takes a dictionary (library) and an ISBN (string) as input, and removes the book with the given ISBN
       from the library. If the ISBN does not exist in the library, print an appropriate message.'''
-    counter = 1
+    count = 1
     for key in library:
-        if library[counter]['ISBN'] == isbn:
-            del library[counter]
+        if library[count]['ISBN'] == isbn:
+            del library[count]
             print('The book is deleted successfully..')
         else:
             print('The ISBN dosn\'nt exist!')
 
-        counter +=1
+        count +=1
 
 
 def check_out_book(library, isbn):
     '''takes a dictionary (library) and an ISBN (string) as input, and sets the 'available' key of the book with
       the given ISBN to False. If the ISBN does not exist in the library or the book is not available,
         print an appropriate message.'''
-    counter = 1
+    count = 1
     for key in library:
-        if library[counter]['ISBN'] != isbn:
+        if library[count]['ISBN'] != isbn:
             print('The book is not available!')
-            library[counter]['Available'] = False
+            library[count]['Available'] = False
 
         else:
-            print(f'The book is available..\n {library[counter]}')
+            print(f'The book is available..\n {library[count]}')
 
         
 
 def return_book(library, isbn):
     '''takes a dictionary (library) and an ISBN (string) as input, and sets the 'available' key of the book with 
     the given ISBN to True. If the ISBN does not exist in the library, print an appropriate message.'''
-    counter = 1
+    count = 1
     for key in library:
-        if library[counter]['ISBN'] == isbn:
-            library[counter]['Available'] = True
+        if library[count]['ISBN'] == isbn:
+            library[count]['Available'] = True
             print('The book is available..')
 
         else:
