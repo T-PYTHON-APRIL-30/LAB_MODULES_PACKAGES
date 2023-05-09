878de07fccc4e7cc65717088af4cab73b37772d2
Rayan Hantoul <ryan-055@outlook.com>
2023-05-09 14:48:55 +0300

    Solved Lap

---
+++ dateOP.py
@@ -0,0 +1,8 @@
+#function to print current date when call it
+def current_date():
+
+    from datetime import date
+
+    date_now = date.today()
+
+    return date_now
---
+++ main.py
@@ -0,0 +1,4 @@
+#import module and call function
+import dateOP
+
+print(f"the current date is:{dateOP.current_date()}")
