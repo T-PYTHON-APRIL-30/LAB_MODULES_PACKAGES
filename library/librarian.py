c98b54efc388bcb12442c3ec992606cb2073248d
amalsaab <441102683@student.ksu.edu.sa>
2023-05-11 08:54:27 +0300

    my solve

--- library/librarian.py
+++ library/librarian.py
@@ -1,11 +1,11 @@
 
-def add_book(library, title, author, isbn):
+def add_book(library : dict, title : str, author:str, isbn:str):
     pass
-def remove_book(library, isbn):
+def remove_book(library : dict, isbn :str):
     pass
-def check_out_book(library, isbn):
+def check_out_book(library : dict, isbn : str):
     pass 
-def return_book(library, isbn):
+def return_book(library : dict, isbn:str):
     pass
-def display_books(library):
+def display_books(library : dict):
     pass
